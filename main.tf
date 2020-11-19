ac304a618adb28d1c2dfc123cb582ef090c10169
Ubuntu <ubuntu@ip-172-31-40-88.eu-west-1.compute.internal>
2020-11-19 18:13:46 +0000

    first commit

---
+++ main.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+  region                  = "eu-west-1"
+  shared_credentials_file = "~/.aws/credentials"
+}
+
+module "rds_mod" {
+  source     = "./rds"
+  rds_sn_id_priv  = module.vpc_mod.private_subnet_id
+  rds_sn_id_pub = module.vpc_mod.public_subnet_id
+  rds_sg_id  = module.vpc_mod.security_group_id
+  rds_vpc_id = module.vpc_mod.id_vpc
+}
+
+module "ec2_mod" {
+  source                = "./ec2"
+  instance_sn_id        = module.vpc_mod.public_subnet_id
+  instance_sg_id        = module.vpc_mod.security_group_id
+  public_ip_association = true
+}
+
+module "vpc_mod" {
+  source = "./vpc"
+}
